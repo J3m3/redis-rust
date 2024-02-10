48ffb041a86ec340d59eea7338a66247a9c83d12
YJS <y.j3ms.n@gmail.com>
2024-02-10 18:41:52 +0900

    refactor: change varaible names
    - from _stream to stream
    - from stream to connection

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main() -> Result<()> {
 
   let listener = TcpListener::bind("127.0.0.1:6379").context("failed to bind to address")?;
 
-  for _stream in listener.incoming() {
-    match _stream {
-      Ok(mut stream) => {
+  for stream in listener.incoming() {
+    match stream {
+      Ok(mut connection) => {
         println!("accepted new connection");
-        handle_connection(&mut stream).context("failed to handle connection")?;
+        handle_connection(&mut connection).context("failed to handle connection")?;
       }
       Err(e) => {
         println!("error: {}", e);
@@ -23,13 +23,13 @@ fn main() -> Result<()> {
   Ok(())
 }
 
-fn handle_connection(stream: &mut TcpStream) -> Result<()> {
+fn handle_connection(connection: &mut TcpStream) -> Result<()> {
   let mut buf = BytesMut::with_capacity(1024);
 
   let response = "+PONG\r\n";
   buf.put(response.as_bytes());
 
-  stream
+  connection
     .write_all(&buf)
     .context("failed to write to stream")?;
 
