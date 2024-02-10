e0cafa6b1d46594de5ca6e6f116bfb94f13541af
YJS <y.j3ms.n@gmail.com>
2024-02-10 18:11:35 +0900

    feat: support PING command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use std::net::TcpListener;
+use anyhow::{Context, Result};
+use bytes::{BufMut, BytesMut};
+use std::{io::Write, net::TcpListener};
 
-fn main() {
-    println!("Logs from your program will appear here!");
+fn main() -> Result<()> {
+  println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+  let listener = TcpListener::bind("127.0.0.1:6379").context("failed to bind to address")?;
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+  for _stream in listener.incoming() {
+    match _stream {
+      Ok(mut stream) => {
+        println!("accepted new connection");
+
+        let mut buf = BytesMut::with_capacity(1024);
+
+        let response = "+PONG\r\n";
+        buf.put(response.as_bytes());
+
+        stream
+          .write_all(&buf)
+          .context("failed to write to stream")?;
+      }
+      Err(e) => {
+        println!("error: {}", e);
+      }
     }
+  }
+
+  Ok(())
 }
